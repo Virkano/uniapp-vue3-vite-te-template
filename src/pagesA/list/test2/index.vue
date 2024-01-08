f7f0c55325b4036c853ad5a9b01d55e9e7222663
Kano <15737228935@163.com>
2024-01-08 15:49:23 +0800

    feat: init

---
+++ src/pagesA/list/test2/index.vue
@@ -0,0 +1,26 @@
+<script lang="ts" setup>
+import { onMounted, ref } from 'vue';
+import BasicButton from '@/components/BasicButton/index.vue';
+import Navbar from '@/components/Navbar/index.vue';
+
+const router = useRouter();
+const jumpDetail = () => {
+    router.push('/pagesB/detail/index?page=1&limit=20');
+};
+const params = ref();
+
+onMounted(() => {
+    params.value = router.route.value.query;
+});
+</script>
+<template>
+    <view>
+        <Navbar title="Test2" />
+        <view class="center"> Test2 </view>
+        <view class="center">参数信息： {{ params }} </view>
+        <view class="center">
+            <BasicButton @click="jumpDetail">Detail → </BasicButton>
+        </view>
+    </view>
+</template>
+<style scoped></style>
---
+++ unocss.config.js
@@ -0,0 +1,59 @@
+/**
+ * unocss defineConfig
+ * @link unocss: https://github.com/unocss/unocss
+ * @link unocss-preset-weapp: https://github.com/MellowCo/unocss-preset-weapp
+ * */
+
+import { defineConfig, presetIcons } from 'unocss';
+import presetWeapp from 'unocss-preset-weapp';
+import { transformerAttributify, transformerClass } from 'unocss-preset-weapp/transformer';
+
+const transformRules = {
+    '.': '-d111-',
+    '/': '-s111-',
+    ':': '-c111-',
+    '%': '-p111-',
+    '!': '-e111-',
+    '#': '-w111-',
+    '(': '-b111l-',
+    ')': '-b111r-',
+    '[': '-f111l-',
+    ']': '-f111r-',
+    $: '-r111-',
+    ',': '-r222-',
+};
+
+export default defineConfig({
+    presets: [
+        // https://github.com/MellowCo/unocss-preset-weapp
+        presetWeapp({
+            nonValuedAttribute: true,
+            whRpx: true,
+            transform: true,
+            platform: 'uniapp',
+            transformRules,
+        }),
+        presetIcons({
+            scale: 1.2,
+            warn: true,
+        }),
+    ],
+    shortcuts: [
+        {
+            'border-base': 'border border-gray-500_10',
+            'z-tar-both': 'z-988',
+            'head-fixed': 'fixed top-0 left-0 w-full z-tar-both',
+            'f-c-c': 'flex justify-center items-center',
+        },
+    ],
+    theme: {},
+    transformers: [
+        transformerAttributify({
+            transformRules,
+            nonValuedAttribute: true,
+        }),
+        transformerClass({
+            transformRules,
+        }),
+    ],
+});
