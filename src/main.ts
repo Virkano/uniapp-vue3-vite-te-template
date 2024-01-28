ffca1200f9ec2eb2af9bd47ad46113f1a0334b3b
zzckano <15737228935@163.com>
2024-01-28 15:35:34 +0800

    fix: eslint

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,19 @@
-import { createSSRApp } from 'vue';
-import App from './App.vue';
-import 'uno.css';
-import { setupStore } from '@/state';
-import { setupRouter } from '@/router';
+import { createSSRApp } from 'vue'
+import App from './App.vue'
+import 'uno.css'
+import { setupStore } from '@/state'
+import { setupRouter } from '@/router'
 
 export function createApp() {
-  const app = createSSRApp(App);
+  const app = createSSRApp(App)
 
   // Configure router
-  setupRouter(app);
+  setupRouter(app)
 
   // Configure store
-  setupStore(app);
+  setupStore(app)
 
   return {
     app,
-  };
+  }
 }
